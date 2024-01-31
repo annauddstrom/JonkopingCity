fcad5626e7eeefdf31ce7b9bdc9c2f884a3d7da3
ElinHenriksson01 <elinhenriksson01@gmail.com>
2024-01-31 11:12:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const express = require('express')
-const ModelClass = require('./model.js');
-const storeJson = require('./stores.json');
-const app = express()
+const express = require("express");
+const ModelClass = require("./model.js");
+const storeJson = require("./stores.json");
+const app = express();
 let Model = null;
 
-app.get('/setup', async (req, res) => {
+app.get("/setup", async (req, res) => {
   await Model.setup(storeJson);
-  res.json({success: true});
+  res.json({ success: true });
 });
 
-app.get('/store/:id', async(req, res) => {
+app.get("/store/:id", async (req, res) => {
   //add digit check {TODO}
-  const store = await Model.getStore(req.params.id)
-  res.json(store)
-})
+  const store = await Model.getStore(req.params.id);
+  res.json(store);
+});
 
-app.get('/storeDistrict/:district', async(req, res) => {
+app.get("/storeDistrict/:district", async (req, res) => {
   //check legit district {TODO}
-  const stores = await Model.getStoresInDistrict(req.params.district)
-  res.json(stores)
-})
+  const stores = await Model.getStoresInDistrict(req.params.district);
+  res.json(stores);
+});
 
-app.get('/stores', async (req, res) => {
+app.get("/stores", async (req, res) => {
   const stores = await Model.getAllStores();
   res.json(stores);
-})
+});
 
 const startServer = async () => {
   Model = new ModelClass();
   await Model.init();
   app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+    console.log("Example app listening on port 3000!");
   });
-}
+};
 
 startServer();
